bdd37590a3ab2ef99abcc6454a63bcca49f56322
Eric Tallant <etallant@degdigital.com>
2025-04-03 07:55:48 -0500

    add headerNav query

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,7 @@
-"use client";
+import * as React from 'react';
+import Link from 'next/link';
 
-import * as React from "react";
-import Link from "next/link";
-
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -11,43 +9,51 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+  navigationMenuTriggerStyle
+} from '@/components/ui/navigation-menu';
 
-import { Icons } from "./icons";
+import { Icons } from './icons';
+import { HEADERNAV_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch } from '@/sanity/lib/live';
 
 const components: { title: string; href: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "#",
+    title: 'Alert Dialog',
+    href: '#'
   },
   {
-    title: "Hover Card",
-    href: "#",
+    title: 'Hover Card',
+    href: '#'
   },
   {
-    title: "Progress",
-    href: "#",
+    title: 'Progress',
+    href: '#'
   },
   {
-    title: "Scroll-area",
-    href: "#",
+    title: 'Scroll-area',
+    href: '#'
   },
   {
-    title: "Tabs",
-    href: "#",
+    title: 'Tabs',
+    href: '#'
   },
   {
-    title: "Tooltip",
-    href: "#",
-  },
+    title: 'Tooltip',
+    href: '#'
+  }
 ];
 
 interface NavProps {
   className: string;
 }
 
-export function Nav({ className }: NavProps) {
+export async function Nav({ className }: NavProps) {
+  const { data: headerNav } = await sanityFetch({
+    query: HEADERNAV_QUERY
+  });
+
+  console.log(headerNav.links);
+
   return (
     <NavigationMenu className={className}>
       <NavigationMenuList>
@@ -62,7 +68,7 @@ export function Nav({ className }: NavProps) {
 const NavLink = () => {
   return (
     <NavigationMenuItem>
-      <Link href='/docs' legacyBehavior passHref>
+      <Link href="/docs" legacyBehavior passHref>
         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
           Documentation
         </NavigationMenuLink>
@@ -76,8 +82,8 @@ const NavSubMenu = () => {
     <NavigationMenuItem>
       <NavigationMenuTrigger>Components</NavigationMenuTrigger>
       <NavigationMenuContent>
-        <ul className='grid w-[300px] gap-3 p-4 md:grid-cols-2 lg:w-[400px] '>
-          {components.map((component) => (
+        <ul className="grid w-[300px] gap-3 p-4 md:grid-cols-2 lg:w-[400px] ">
+          {components.map(component => (
             <ListItem
               key={component.title}
               title={component.title}
@@ -95,29 +101,29 @@ const NavCard = () => {
     <NavigationMenuItem>
       <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
       <NavigationMenuContent>
-        <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-          <li className='row-span-3'>
+        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+          <li className="row-span-3">
             <NavigationMenuLink asChild>
               <a
-                className='flex flex-col justify-end w-full h-full p-6 no-underline rounded-md outline-none select-none bg-gradient-to-b from-muted/50 to-muted focus:shadow-md hover:bg-gray-200'
-                href='/'
+                className="flex flex-col justify-end w-full h-full p-6 no-underline rounded-md outline-none select-none bg-gradient-to-b from-muted/50 to-muted focus:shadow-md hover:bg-gray-200"
+                href="/"
               >
                 <Icons.rocket height={50} width={50} />
-                <div className='mb-2 mt-4 text-lg font-medium'>Web Design</div>
-                <p className='text-sm leading-tight text-muted-foreground'>
+                <div className="mb-2 mt-4 text-lg font-medium">Web Design</div>
+                <p className="text-sm leading-tight text-muted-foreground">
                   Beautifully designed components that you can copy and paste
                   into your apps. Accessible. Customizable. Open Source.
                 </p>
               </a>
             </NavigationMenuLink>
           </li>
-          <ListItem href='#' title='Introduction'>
+          <ListItem href="#" title="Introduction">
             Re-usable components built using Radix UI and Tailwind CSS.
           </ListItem>
-          <ListItem href='#' title='Installation'>
+          <ListItem href="#" title="Installation">
             How to install dependencies and structure your app.
           </ListItem>
-          <ListItem href='#' title='Typography'>
+          <ListItem href="#" title="Typography">
             Styles for headings, paragraphs, lists...etc
           </ListItem>
         </ul>
@@ -127,8 +133,8 @@ const NavCard = () => {
 };
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
@@ -136,13 +142,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "w-full block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'w-full block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
           {...props}
         >
-          <div className='text-sm font-medium leading-none'>{title}</div>
-          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
         </a>
@@ -150,4 +156,4 @@ const ListItem = React.forwardRef<
     </li>
   );
 });
-ListItem.displayName = "ListItem";
+ListItem.displayName = 'ListItem';
