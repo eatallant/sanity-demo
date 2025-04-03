cf360d21b75d330a388a84218c46561010798b74
Eric Tallant <etallant@degdigital.com>
2025-04-03 15:10:16 -0500

    set up media plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,19 @@
-'use client'
+'use client';
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
-
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { presentationTool } from 'sanity/presentation';
+import { media } from 'sanity-plugin-media';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { apiVersion, dataset, projectId } from './src/sanity/env';
+import { schema } from './src/sanity/schemaTypes';
+import { structure } from './src/sanity/structure';
 
 export default defineConfig({
   basePath: '/studio',
@@ -22,18 +22,19 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     presentationTool({
       previewUrl: {
         origin: process.env.NEXT_PUBLIC_SANITY_STUDIO_URL,
-        preview: "/",
+        preview: '/',
         previewMode: {
-          enable: "/api/draft-mode/enable",
-        },
-      },
+          enable: '/api/draft-mode/enable'
+        }
+      }
     }),
+    media(),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-})
+    visionTool({ defaultApiVersion: apiVersion })
+  ]
+});
